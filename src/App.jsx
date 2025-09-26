ee1a03318c686aad879bd3ed749d3829ed2d3f63
jahidur17 <jahidurrahman9117@gmail.com>
2025-09-27 03:28:24 +0600

    added the function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Suspense } from 'react';
+import { Suspense,useState, useEffect} from 'react';
 import './App.css'
 import Card from './components/Card';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import TicketsCard from './components/TicketsCard/TicketsCard';
+
 
 
 const fetchTickets = async() => {
@@ -12,16 +13,46 @@ const fetchTickets = async() => {
 }
 
 function App() {
+
+
+  const [ticketsValue, setTicketsData] = useState([]);
+  console.log(ticketsValue);
+  const [inProgressCount, setInProgressCount] = useState(0);
+  const [resolvedCount, setResolvedCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/tickets.json")
+      .then((res) => res.json())
+      .then((data) => setTicketsData(data));
+  }, []);
+
+  const handleCardClick = (id) => {
+    if (id.includes(id)) {
+      setInProgressCount((prev) => prev + 1);
+    } else if (id === "Resolved") {
+      setResolvedCount((prev) => prev + 1);
+    }
+  };
+
   
+
  const ticketsPromise = fetchTickets()
+
+
   return (
     <>
       <Navbar></Navbar>
-      <Card></Card>
-      <Suspense
-        fallback={<span className="loading loading-spinner loading-xl"></span>}
-      >
-        <TicketsCard ticketsPromise={ticketsPromise}></TicketsCard>
+      <Card handleCardClick={handleCardClick} resolvedCount={resolvedCount} inProgressCount={inProgressCount}></Card>
+      <div className="flex max-w-[1200px] mx-auto ">
+        <h2 className="w-3/4 font-semibold text-xl text-[#34485A] ">
+          Customer Tickets
+        </h2>
+        <h2 className="w-1/4 font-semibold text-xl text-[#34485A]">
+          Task Status
+        </h2>
+      </div>
+      <Suspense>
+        <TicketsCard handleCardClick={handleCardClick}  ticketsPromise={ticketsPromise}></TicketsCard>
       </Suspense>
       <Footer></Footer>
     </>
--- src/components/TicketsCard/TicketsCard.jsx
+++ src/components/TicketsCard/TicketsCard.jsx
@@ -2,26 +2,48 @@ import React, { use } from 'react';
 import ellipse22 from '../../assets/Ellipse 22.png'
 import jahid from '../../assets/jk.png'
 
-const TicketsCard = ({ ticketsPromise }) => {
+const TicketsCard = ({ ticketsPromise, handleCardClick }) => {
   const ticketsData = use(ticketsPromise);
-  console.log(ticketsData);
+//   console.log(ticketsData);
   return (
-    <div className="max-w-[1200px] mx-auto grid md:grid-cols-4 grid-rows-1 ">
+    <div className="max-w-[1200px] mx-auto grid md:grid-cols-4 grid-rows-1 gap-5">
       <div className="col-span-3 grid md:grid-cols-2 grid-rows-1  gap-5 ">
         {ticketsData.map((ticket) => (
-          <div className="p-1 shadow-lg rounded-md">
+          <div
+            key={ticket.id}
+            onClick={() =>handleCardClick(ticket.status)}
+            className="p-1 shadow-lg rounded-md cursor-pointer"
+          >
             <div className="flex justify-between items-center">
               <h2 className="font-semibold">{ticket.title}</h2>
-              <div className="flex justify-center items-center space-x-2 btn rounded-full bg-[#B9F8CF]">
+              <div
+                className={`flex justify-center items-center space-x-2 btn rounded-full  ${
+                  ticket.status == "In Progress"
+                    ? "text-[#c1bdb0] bg-[#9e843b]"
+                    : ticket.status == "To Do"
+                    ? "text-blue-100 bg-blue-500"
+                    : "text-gray-100 bg-gray-500"
+                } `}
+              >
                 <img className="" src={ellipse22} alt="" />
-                <p className="font-semibold ">{ticket.status}</p>
+                <p className={`font-semibold  `}>{ticket.status}</p>
               </div>
             </div>
             <p className="mt-3 text-[#627382]">{ticket.description}</p>
             <div className="flex justify-between items-center mt-3">
               <div className="flex space-x-4">
                 <span className="text-[#627382]">{ticket.id}</span>
-                <span>{ticket.priority}</span>
+                <span
+                  className={
+                    ticket.priority == "High"
+                      ? "text-red-500 bg-red-100"
+                      : ticket.priority == "Medium"
+                      ? "text-yellow-500 bg-yellow-100"
+                      : "text-green-500 bg-green-100"
+                  }
+                >
+                  {ticket.priority}
+                </span>
               </div>
               <div className="flex space-x-4">
                 <span className="text-[#627382]">{ticket.name}</span>
@@ -34,8 +56,19 @@ const TicketsCard = ({ ticketsPromise }) => {
           </div>
         ))}
       </div>
-      <div className="col-span-1 border-2 border-red-400">
-        <h1>i lobe bangla hhjmsbdsb ajsjnmd cbbayjchc </h1>
+
+      <div className="col-span-1 ">
+        <div className="shadow-md p-2 mt-4 rounded-md ">
+          <h1 className="flex justify-center items-center font-semibold ">
+            Payment Failed - Card Declined
+          </h1>
+          <button
+            
+            className="btn text-white w-[270px] h-[50px] bg-[#02A53B] rounded-md mt-3"
+          >
+            Complete
+          </button>
+        </div>
       </div>
     </div>
   );
